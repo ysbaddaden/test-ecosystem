d6b0cd9be04b3f8789b2d0517df19dbd3b8bacb4
Julien Portalier <julien@portalier.com>
2025-10-29 23:38:00 +0100

    Use installed postgres

--- src/gha.cr
+++ src/gha.cr
@@ -52,10 +52,11 @@ jobs.each do |steps|
     }
   }
   steps << Step{
-    "uses" => "ikalnytskyi/action-setup-postgres@v8",
-    "with" => {
-      "postgres-version" => "16",
-    }
+    #"uses" => "ikalnytskyi/action-setup-postgres@v8",
+    #"with" => {
+    #  "postgres-version" => "16",
+    #}
+    "uses" => "./.github/actions/postgresql",
   }
 end
 
