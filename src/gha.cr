5df140324348758ea14152dc6e6776d3318daee9
Julien Portalier <julien@portalier.com>
2026-01-22 16:12:20 +0100

    fixup! back to lib.exe call (alt still segfault)

--- src/gha.cr
+++ src/gha.cr
@@ -54,8 +54,7 @@ module GHA
           "run" => "choco install sqlite",
         },
         Step{
-          # "run" => "lib.exe /DEF:sqlite3.def /OUT:sqlite3.lib",
-          "run" => "llvm-dlltool.exe -d sqlite3.def -D sqlite3.dll -l sqlite3-dynamic.lib",
+          "run" => "lib.exe /DEF:sqlite3.def /OUT:sqlite3-dynamic.lib",
           "working-directory" => "C:\\ProgramData\\chocolatey\\lib\\SQLite\\tools",
         },
         Step{
